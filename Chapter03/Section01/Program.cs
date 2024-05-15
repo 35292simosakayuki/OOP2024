f81131821938ff502ade775abe99022fba7d6356
simosakayuki <166583450+35292simosakayuki@users.noreply.github.com>
2024-05-15 11:49:38 +0900

    ラムダ式

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+
+
+           // int count = Count(numbers,  n=> n%2==0);
+           double num=numbers.Where(n=>n>5).Average();
+           int total = numbers.Where(n => n>5).Sum();
+            Console.WriteLine(num);
+            Console.WriteLine(total);
+        
+
+        
+        }
+    }
+}
