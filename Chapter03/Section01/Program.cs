3d522d9fc09117f79acfd632865b763f54789a2b
simosakayuki <166583450+35292simosakayuki@users.noreply.github.com>
2024-05-15 15:21:06 +0900

    演習問題

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -8,17 +8,19 @@ using System.Threading.Tasks;
 namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-
-
-           // int count = Count(numbers,  n=> n%2==0);
-           double num=numbers.Where(n=>n>5).Average();
-           int total = numbers.Where(n => n>5).Sum();
-            Console.WriteLine(num);
-            Console.WriteLine(total);
-        
-
-        
+            var names = new List<string> {
+              "Tokyo",
+              "New Delhi",
+              "Bangkok",
+              "Paris",
+              "Bariln",
+              "Canberra",
+              "Hong kong",
+          };
+            IEnumerable<string>query=names.Where(s=>s.Contains(' '))
+                .Select(s=>s.ToUpper());
+            foreach(string s in query)
+                Console.WriteLine(s);
         }
     }
 }
