cf328abfcd739d2e501d3eb7dc141182ed100f2d
simosakayuki <166583450+35292simosakayuki@users.noreply.github.com>
2024-09-03 11:37:19 +0900

    btGet_Click

---
+++ FormApps/RssReader/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace RssReader {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.tbRssUrl = new System.Windows.Forms.TextBox();
+            this.btGet = new System.Windows.Forms.Button();
+            this.lbRssTitle = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // tbRssUrl
+            // 
+            this.tbRssUrl.Location = new System.Drawing.Point(39, 20);
+            this.tbRssUrl.Name = "tbRssUrl";
+            this.tbRssUrl.Size = new System.Drawing.Size(523, 19);
+            this.tbRssUrl.TabIndex = 0;
+            // 
+            // btGet
+            // 
+            this.btGet.Location = new System.Drawing.Point(568, 18);
+            this.btGet.Name = "btGet";
+            this.btGet.Size = new System.Drawing.Size(75, 23);
+            this.btGet.TabIndex = 1;
+            this.btGet.Text = "取得";
+            this.btGet.UseVisualStyleBackColor = true;
+            // 
+            // lbRssTitle
+            // 
+            this.lbRssTitle.FormattingEnabled = true;
+            this.lbRssTitle.ItemHeight = 12;
+            this.lbRssTitle.Location = new System.Drawing.Point(39, 58);
+            this.lbRssTitle.Name = "lbRssTitle";
+            this.lbRssTitle.Size = new System.Drawing.Size(604, 364);
+            this.lbRssTitle.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbRssTitle);
+            this.Controls.Add(this.btGet);
+            this.Controls.Add(this.tbRssUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbRssUrl;
+        private System.Windows.Forms.Button btGet;
+        private System.Windows.Forms.ListBox lbRssTitle;
+    }
+}
+
---
+++ FormApps/RssReader/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private void btGet_Click(object sender, EventArgs e) {
+            using(var wc=new WebClient()) {
+                var url=wc.OpenRead(tbRssUrl.Text);
+                var xdoc=XDocument.Load(url);
+
+                var xelements = xdoc.Root.Descendants("item");
+
+
+
+            }
+        }
+    }
+}
