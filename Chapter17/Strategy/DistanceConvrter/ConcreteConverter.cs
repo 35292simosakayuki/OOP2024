0fda3497b1c52ed7214c7fe5787109b5f708ab1f
UchidaHaruto <oibc35159@ojs.ac.jp>
2024-10-10 10:53:28 +0900

    17-2

---
+++ Chapter17/Strategy/DistanceConvrter/ConcreteConverter.cs
@@ -0,0 +1,57 @@
+﻿using DistanceConverter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    // List 17-13 (ConverterBaseの具象クラス)
+
+    public class MeterConverter : ConverterBase {
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "meter" || name == UnitName;
+        }
+        protected override double Ratio { get { return 1; } }
+        public override string UnitName { get { return "メートル"; } }
+    }
+
+    public class FeetConverter : ConverterBase {
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "feet" || name == UnitName;
+        }
+        protected override double Ratio { get { return 0.3048; } }
+        public override string UnitName { get { return "フィート"; } }
+    }
+
+    public class InchConverter : ConverterBase {
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "inch" || name == UnitName;
+        }
+        protected override double Ratio { get { return 0.0254; } }
+        public override string UnitName { get { return "インチ"; } }
+    }
+
+    public class YardConverter : ConverterBase {
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "yard" || name == UnitName;
+        }
+        protected override double Ratio { get { return 0.9144; } }
+        public override string UnitName { get { return "ヤード"; } }
+    }
+}
+public class MileConverter : ConverterBase {
+    public override bool IsMyUnit(string name) {
+        return name.ToLower() == "mile" || name == UnitName;
+    }
+    protected override double Ratio { get { return 1609.344; } }
+    public override string UnitName { get { return "マイル"; } }
+}
+public class KiloConverter : ConverterBase {
+    public override bool IsMyUnit(string name) {
+        return name.ToLower() == "kilometer" || name == UnitName;
+    }
+    protected override double Ratio { get { return 1000; } }
+    public override string UnitName { get { return "キロメートル"; } }
+}
+
---
+++ Chapter17/Strategy/DistanceConvrter/ConverterFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    static class ConverterFactory {
+        private static ConverterBase[] _converters = new ConverterBase[] {
+            new MeterConverter() ,
+            new FeetConverter() ,
+            new YardConverter() ,
+            new InchConverter(),
+            new MileConverter() ,
+            new KiloConverter() ,
+             
+        };
+        public static ConverterBase GetInstance(string name) {
+            return _converters.FirstOrDefault(x => x.IsMyUnit(name));
+        }
+    }
+}
