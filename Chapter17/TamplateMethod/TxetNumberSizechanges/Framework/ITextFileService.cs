4e41814698bcae57592ab1b4bb2e9a1856d3cd2a
UchidaHaruto <oibc35159@ojs.ac.jp>
2024-10-16 15:06:35 +0900

    17-1完成

---
+++ Chapter17/TamplateMethod/TxetNumberSizechanges/Framework/ITextFileService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextNumberSizeChange.Framework {
+    public interface ITextFileService {
+        void Initialize(string fname);
+        void Execute(string line);
+        void Terminate();
+    }
+    public class TextFileProcessor {
+        private ITextFileService _service;
+
+        public TextFileProcessor(ITextFileService service) {
+            _service = service;
+        }
+            public void Run(string fname) {
+                _service.Initialize(fname);
+                using (var sr = new StreamReader(fname)) {
+                    while (!sr.EndOfStream) {
+                        string line = sr.ReadLine();
+                        _service.Execute(line);
+                    }
+                }
+                _service.Terminate();
+            }
+        }
+
+    }
+
+
+
---
+++ Chapter17/TamplateMethod/TxetNumberSizechanges/ToHankakuProcessor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileProcessor;
+using TextNumberSizeChange.Framework;
+
+namespace TextNumberSizeChange {
+    class ToHankakuProcessor : ITextFileService {
+
+        private int _count;
+        private StringBuilder _text = new StringBuilder();
+
+        void ITextFileService.Initialize(string fname) {
+            _count = 0;
+        }
+
+        void ITextFileService.Execute(string line) {
+            string convertedLine = ConvertToHankakuNumbers(line);
+            _text.AppendLine(convertedLine); // 変換し追加する
+            _count++;
+        }
+
+        void ITextFileService.Terminate() {
+            Console.WriteLine("{0} 行", _count);
+            Console.WriteLine(_text.ToString());
+        }
+
+        private string ConvertToHankakuNumbers(string input) {
+            // 全角数字と半角数字の割当表
+            char[] fullWidthDigits = { '０', '１', '２', '３', '４', '５', '６', '７', '８', '９' };
+            char[] halfWidthDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            for (int i = 0; i < fullWidthDigits.Length; i++) {
+                input = input.Replace(fullWidthDigits[i], halfWidthDigits[i]);
+            }
+
+            return input;
+        }
+
+        
+    }
+}
